1ae17d9e10aa1b813b91c8aa6b1a6614008f8b9e
Andey Amelchenia <pmad1986@gmail.com>
2020-10-23 13:16:23 +0300

    init: start test

---
+++ intervals.js
@@ -0,0 +1,28 @@
+function intervalConstruction(arr) {
+}
+
+function intervalIdentification(arr) {
+}
+
+intervalConstruction(['M2', 'C', 'asc']);
+intervalConstruction(['P5', 'B', 'asc']);
+intervalConstruction(['m2', 'Bb', 'dsc']);
+intervalConstruction(['M3', 'Cb', 'dsc']);
+intervalConstruction(['P4', 'G#', 'dsc']);
+intervalConstruction(['m3', 'B', 'dsc']);
+intervalConstruction(['m2', 'Fb', 'asc']);
+intervalConstruction(['M2', 'E#', 'dsc']);
+intervalConstruction(['P4', 'E', 'dsc']);
+intervalConstruction(['m2', 'D#', 'asc']);
+intervalConstruction(['M7', 'G', 'asc']);
+
+intervalIdentification(['C', 'D']);
+intervalIdentification(['B', 'F#', 'asc']);
+intervalIdentification(['Fb', 'Gbb']);
+intervalIdentification(['G', 'F#', 'asc']);
+intervalIdentification(['Bb', 'A', 'dsc']);
+intervalIdentification(['Cb', 'Abb', 'dsc']);
+intervalIdentification(['G#', 'D#', 'dsc']);
+intervalIdentification(['E', 'B', 'dsc']);
+intervalIdentification(['E#', 'D#', 'dsc']);
+intervalIdentification(['B', 'G#', 'dsc']);
